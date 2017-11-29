f7964fd7872a524225d4e4d345fa96a1053e3420
Lenora Rigoni <lenorarigoni@gmail.com>
2017-11-29 08:23:29 -0500

    nav order

--- src/components/SubNav.js
+++ src/components/SubNav.js
@@ -13,11 +13,12 @@ class SubNav extends Component {
                     <a className="subnav-item" href="/projects/2">Offkilter Ceramics</a>
                     <a className="subnav-item" href="/projects/3">Babyganics</a>
                     <a className="subnav-item" href="/projects/4">NYBG Impressify</a>
-                    <a className="subnav-item" href="/projects/5">Max Weinman</a>
-                    <a className="subnav-item" href="/projects/6">Madway Holiday Gift</a>
-                    <a className="subnav-item" href="/projects/7">Thingmade</a>
+                    <a className="subnav-item" href="/projects/5">FUR</a>
+                    <a className="subnav-item" href="/projects/6">Max Weinman</a>
+                    <a className="subnav-item" href="/projects/7">Madway Holiday Gift</a>
                     <a className="subnav-item" href="/projects/8">Artlook</a>
                     <a className="subnav-item" href="/projects/9">NYBG Frida Kahlo</a>
+                    <a className="subnav-item" href="/projects/10">Thingmade</a>
                 </div>
             </div>   
         )    
